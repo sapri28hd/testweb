7d749cc7d58f9ad54ffa242e2f60ebbe40d46254
sapri28hd <6306021422127@fitm.kmutnb.ac.th>
2021-03-09 12:03:11 +0700

    https://github.com/sapri28hd/testweb.git

--- db.php
+++ db.php
@@ -1,2 +1,16 @@
 <?php
+    define("hostname","localhost");
+    define("user","root");
+    define("password","");
+    define("dbConn","bookstore");
+?>
+<?php
+    $mysqli = new mysqli((hostname,user,password,dbConn);
+    
+    // Check connection
+    if ($mysqli -> connect_errno) {
+      echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+      exit();
+    }
+
 ?>
